616db2e0a922a7fe6e824b9549d8b6622cbdb1fc
Martin Kreibe <mkreibe@astraflux.com>
2017-08-23 07:57:54 -0600

    [WIP] fixed various layout issues, added spinner, small cleanups.

--- helpers/routes.go
+++ helpers/routes.go
@@ -1,7 +1,11 @@
 package helpers
 
 import (
+	"html/template"
+	"log"
 	"net/http"
+	"path/filepath"
+	"strings"
 )
 
 // APIType defines the type of API to use.
@@ -19,6 +23,16 @@ const (
 	None = APIType("None")
 )
 
+// navItems holds the collection of navigation items.
+var navItems []NavItem
+
+// NavItem defines the navigation items.
+type NavItem struct {
+	Title   string
+	Current bool
+	Link    string
+}
+
 // RouteDefinition stores information about the route.
 type RouteDefinition struct {
 	Method     string
@@ -26,4 +40,72 @@ type RouteDefinition struct {
 	APIType    APIType
 	Handler    func(api API) http.HandlerFunc
 	RawHandler http.Handler
+}
+
+// StaticFiles creates a static site.
+func StaticFiles(parts ...string) (route RouteDefinition) {
+
+	sep := "/"
+	path := strings.Join(parts, sep) + sep
+
+	return RouteDefinition{
+		Method:     http.MethodGet,
+		Path:       sep + path + "{rest}",
+		APIType:    None,
+		RawHandler: http.StripPrefix(sep+path, http.FileServer(http.Dir(path))),
+	}
+}
+
+// TemplateRoute defines the route
+func TemplateRoute(name string, path string, definition string) (route RouteDefinition) {
+
+	navItems = append(navItems, NavItem{
+		Title: name,
+		Link:  path,
+	})
+
+	return RouteDefinition{
+		Method:  http.MethodGet,
+		Path:    path,
+		APIType: OpsMan,
+		Handler: func(api API) http.HandlerFunc {
+			return func(w http.ResponseWriter, r *http.Request) {
+
+				paths := []string{
+					filepath.Join("static", definition),
+					filepath.Join("static", "layout.html"),
+				}
+
+				var err error
+				var tmpl *template.Template
+				if tmpl, err = template.ParseFiles(paths...); err == nil {
+
+					data := struct {
+						Title    string
+						NavItems []NavItem
+					}{
+						Title:    name,
+						NavItems: make([]NavItem, len(navItems)),
+					}
+
+					copy(data.NavItems, navItems)
+					for index, item := range data.NavItems {
+
+						if item.Title == name {
+							data.NavItems[index].Current = true
+							break
+						}
+					}
+
+					err = tmpl.ExecuteTemplate(w, "layout", data)
+				}
+
+				if err != nil {
+					status := http.StatusNotFound
+					log.Println(err.Error())
+					http.Error(w, http.StatusText(status), status)
+				}
+			}
+		},
+	}
 }
--- routes.go
+++ routes.go
@@ -1,71 +1,9 @@
 package main
 
 import (
-	"html/template"
-	"log"
-	"net/http"
-	"path/filepath"
-	"strings"
-
 	"github.com/ECSTeam/pcf-status/helpers"
 	"github.com/ECSTeam/pcf-status/models"
 )
-
-var (
-	coreFragments = []string{"nav.html", "scripts.html", "layout.html"}
-)
-
-// staticFiles creates a static site.
-func staticFiles(parts ...string) (route helpers.RouteDefinition) {
-
-	sep := "/"
-	path := strings.Join(parts, sep) + sep
-
-	return helpers.RouteDefinition{
-		Method:     http.MethodGet,
-		Path:       sep + path + "{rest}",
-		APIType:    helpers.None,
-		RawHandler: http.StripPrefix(sep+path, http.FileServer(http.Dir(path))),
-	}
-}
-
-// templateRoute defines the route
-func templateRoute(path string, parts ...string) (route helpers.RouteDefinition) {
-	return helpers.RouteDefinition{
-		Method:  http.MethodGet,
-		Path:    path,
-		APIType: helpers.OpsMan,
-		Handler: func(api helpers.API) http.HandlerFunc {
-			return func(w http.ResponseWriter, r *http.Request) {
-
-				var paths []string
-				for _, part := range parts {
-					paths = append(paths, filepath.Join("static", part))
-				}
-
-				for _, part := range coreFragments {
-					paths = append(paths, filepath.Join("static", part))
-				}
-
-				if tmpl, err := template.ParseFiles(paths...); err == nil {
-
-					temp, data := helpers.MakeData(api.(*helpers.OpsManAPI))
-					if err = tmpl.ExecuteTemplate(w, temp, data); err != nil {
-						log.Println(err.Error())
-						http.Error(w, http.StatusText(500), 500)
-					}
-
-				} else {
-					// Log the detailed error
-					log.Println(err.Error())
-					// Return a generic "Internal Server Error" message
-					http.Error(w, http.StatusText(500), 500)
-					return
-				}
-			}
-		},
-	}
-}
 
 var (
 	routes = []helpers.RouteDefinition{
@@ -77,9 +15,12 @@ var (
 		models.AppsManBuildpacksRoute,
 		models.AppsManStemcellsRoute,
 		models.AppsManInfoRoute,
-		staticFiles("static", "js"),
-		staticFiles("static", "css"),
-		templateRoute("/", "default.html"),
-		templateRoute("/releases", "releases.html"),
+		helpers.StaticFiles("static", "fonts"),
+		helpers.StaticFiles("static", "js"),
+		helpers.StaticFiles("static", "css"),
+		helpers.TemplateRoute("Home", "/", "default.html"),
+		helpers.TemplateRoute("Releases", "/releases", "releases.html"),
+		helpers.TemplateRoute("Stemcells", "/stemcells", "stemcells.html"),
+		helpers.TemplateRoute("VMs", "/vms", "vms.html"),
 	}
 )
